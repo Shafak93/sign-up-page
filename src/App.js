350fd4300a4b479016aa2c72e13dda0a15bd1d68
Shafak93 <shafakctg64@gmail.com>
2022-08-08 22:13:30 +0600

    added icon, background added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './Components/Signup/Signup';
 
+
 function App() {
   return (
     <div className="App">
       <Signup />
+      
     </div>
   );
 }
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import image from '../../image/dummy.png'
+import './Signup.css'
+import { AiFillGoogleCircle } from 'react-icons/ai';
+import { BsFacebook } from 'react-icons/bs';
+import { AiFillTwitterCircle } from 'react-icons/ai';
 
 
 const Signup = () => {
     return (
-        <div >
-            <div className='flex justify-center items-center mt-10'>
+        <div>
+            <div className='signup mx-32 p-5 flex justify-center items-center my-10   shadow-lg shadow-gray-400 rounded-md'>
                 <div className='mr-10'>
                     <img className='w-96' src={image} alt="Album" />
                 </div>
                 <div>
-                    <h2 class=" text-orange-500 text-3xl font-semibold">Signup</h2>
+                    <h2 class="text-3xl font-semibold text-gray-100 mb-5">Signup with</h2>
+                    <div className='flex ml-28'>
+                    <AiFillGoogleCircle className='bg-gray-100 w-12 h-12 p-1 rounded-full text-orange-500 cursor-pointer' size={30}></AiFillGoogleCircle>
+                    <BsFacebook className='ml-3 bg-gray-100  w-12 h-12 p-2 rounded-full text-orange-500 cursor-pointer' size={30}></BsFacebook> 
+                    <AiFillTwitterCircle className='ml-3 bg-gray-100  w-12 h-12 p-1 rounded-full text-orange-500 cursor-pointer' size={30}></AiFillTwitterCircle> 
+                    </div>
+                    <div class="flex flex-col w-full border-opacity-50">
+                    
+                    <div class="divider ">OR</div>
+                    <h3 className='text-orange-600 text-xl'>Create an account for free</h3>
+                    </div>
+
                 <form action="#" class="p-0">
                 <div class="mt-5 flex">
                     <input type="text" class="block w-44 p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent " placeholder="First name" />
@@ -18,6 +33,9 @@ const Signup = () => {
                 </div>
                 <div class="mt-5">
                     <input type="text" class="block w-96 p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent " placeholder="Email" />
+                </div>
+                <div class="mt-5">
+                    <input type="date" class="block w-96 p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent " placeholder="Date of birth" />
                 </div>
                 <div class="mt-5">
                     <input type="text" class="block w-96 p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent " placeholder="User-name" />
@@ -29,7 +47,7 @@ const Signup = () => {
                     <input type="password" class="block w-96 p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent  " placeholder="Password" />
                 </div>
 
-                <div class="mt-6 block w-96 p-5 text-sm md:font-sans  text-gray-800">
+                <div class="my-6 block w-96 text-sm  text-gray-800">
                     <input type="checkbox" class="inline-block border-0 mr-3 " />
                     <span display="inline">By creating an account you are agreeing to our 
                         <a  href="#" target="_blank" data-test="Link">
